f3a6041e0698d8919501c73e0321145daf979162
yuju-00 <yooju00@naver.com>
2024-03-19 19:40:28 +0900

    기본 기능 구현

--- app.py
+++ app.py
@@ -1,10 +1,80 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request, session
+from flask.json.provider import JSONProvider
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+
+from pymongo import MongoClient
+from bs4 import BeautifulSoup
+from init_db import add_workbook_db, add_study_db
+
+import requests
+import json
+import sys
 
 app = Flask(__name__)
+jwt = JWTManager(app)
 
+client = MongoClient('localhost', 27017)
+db = client.studygram
+
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        return json.JSONEncoder.default(self, o)
+
+class CustomJSONProvider(JSONProvider):
+    def dumps(self, obj, **kwargs):
+        return json.dumps(obj, **kwargs, cls=CustomJSONEncoder)
+
+    def loads(self, s, **kwargs):
+        return json.loads(s, **kwargs)
+
+app.json = CustomJSONProvider(app)
+
+# API #1: 메인 페이지
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('index.html')
+
+# API #2: 로그인 기능 구현
+
+
+# API #3: 스터디 메인 페이지
+@app.route('/study/<int:study_number>', methods=['GET'])
+def study_main(study_number):
+    # 스터디 번호를 이용하여 스터디 정보를 렌더링
+    return render_template('study_main.html', study_number=study_number)
+
+# API #4: 스터디 추가
+@app.route('/study/add', methods=['POST'])
+def add_study():
+    study_title = request.form['study_title']
+    description = request.form['description']
+    date = request.form['date']
+    
+    add_study_db(study_title, description, date)
+    return 'Study added successfully! Study ID: {study_id}'
+
+# 언어에 따른 언어 ID 매핑
+LANGUAGE_IDS = {
+    'C++': 1001,
+    'Java': 1002,
+    'Python': 1003,
+    'C': 1004,
+    'Rust': 1005
+}
+
+# API #5: 스터디에 문제집 추가
+@app.route('/workbook/add/<int:study_number>', methods=['POST'])
+def add_workbook(study_number):
+    workbook_number = request.form['workbook_number']
+    language = request.form['language']
+    username = session.get['username']
+    
+    language_id = LANGUAGE_IDS.get(language)
+    if language_id is None:
+        return 'Invalid Language', 400
+    
+    add_workbook_db(username, study_number, workbook_number, language_id)
+    return 'Workbook added successfully!'
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ init_db.py
@@ -0,0 +1,128 @@
+import random
+import requests
+import threading
+import time
+
+from flask import Flask
+app = Flask(__name__)
+
+from bs4 import BeautifulSoup
+from pymongo import MongoClient  
+
+client = MongoClient('localhost', 27017)
+db = client.studygram
+collection = db['study']
+
+global username
+username = None
+
+# 1. 데이터베이스에 스터디북 추가
+def add_study_db(title, description, date):
+    try:
+        study_count = collection.count_documents({})
+        study_number = study_count + 1
+        
+        study = {
+            'study_number': study_number,
+            'study_title': title,
+            'date': date,
+            'description': description
+        }
+        
+        result = collection.insert_one(study)
+        study_id = result.inserted_id
+        
+        print("Study data updated and saved to MongoDB")
+        print("New study ID: ", study_id)
+        
+    except Exception as e:
+        print("Error:", e)
+
+# 2. 스터디 북에 문제집 추가
+def add_workbook_db(username, study_number, workbook_number, language_id):
+    try:
+        # 백준 문제집 정보 크롤링 (제목, 문제 수)
+        headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+        url = f"https://www.acmicpc.net/workbook/view/{workbook_number}"
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        title_element = soup.select_one('body > div.wrapper > div.container.content > div.row > div:nth-child(2) > div > h1 > span:nth-child(1)')
+        if title_element:
+            problem_title = title_element.text.strip()
+            # print(problem_title)
+        else:
+            problem_title = None
+        
+        problem_list = soup.select('body > div.wrapper > div.container.content > div.row > div:nth-child(3) > div > table > tbody > tr')
+        problem_numbers = []
+        
+        for tr in problem_list:
+            td = tr.find('td')
+            problem_number = td.text.strip()
+            problem_numbers.append(problem_number)
+            
+        total_problems = len(problem_numbers)
+        
+        # 해당 문제집의 진행률 크롤링 (언어별 & 사용자별)
+        url = f'https://www.acmicpc.net/status?problem_id=&user_id={username}&language_id={language_id}'
+        total_success_count = 0
+        
+        # 모든 페이지 크롤링
+        while url:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            
+            soup = BeautifulSoup(response.text, 'html.parser')
+            
+            submission_rows = soup.select('tr[id^="solution"]')
+            if not submission_rows:
+                break  # 페이지가 존재하지 않으면 중단
+            
+            for row in submission_rows:
+                success_count = 0
+                for problem_number in problem_numbers:
+                    problem_element = soup.select_one(f'a[href="/problem/{problem_number}"]')
+                    
+                    if problem_element:
+                        result_element = problem_element.find_next('span', class_='result-text')
+                        
+                        if result_element and 'result-ac' in result_element.get('class', []):
+                            success_count += 1
+                            
+            total_success_count += success_count
+            url = get_next_page_url(response)   # 다음 페이지 크롤링
+        
+        success_ratio = 100 * (total_success_count / total_problems) if total_problems > 0 else 0
+        # print(f'{total_success_count}/{total_problems}')
+        # print(f'{success_ratio}%')
+
+        # MongoDB에 데이터 저장
+        workbook = {
+            'workbook_number': workbook_number,
+            'workbook_title': problem_title,
+            'success_ratio': success_ratio,
+        }
+        
+        collection.update_one(
+            {'study_number': study_number},
+            {'$push': {'workbooks': workbook}}
+        )
+        # print("Workbook data saved to Study number: ", study_number)
+    
+    except Exception as e:
+        print("Error:", e)
+
+# 모든 페이지의 데이터를 크롤링하기 (다음 페이지로 이동하는 함수)
+def get_next_page_url(response):
+    soup = BeautifulSoup(response.text, 'html.parser')
+    next_page_link = soup.select_one('a#next_page')
+    if next_page_link:
+        return 'https://www.acmicpc.net' + next_page_link['href']
+    return None
+
+# 함수 호출 예시
+# add_study_db('기초 스터디', '초보 탈출 기원', '2024.08.19 - 2024.09.19') 
+# add_workbook_db('yooju00', 3, 9528, 1003)
